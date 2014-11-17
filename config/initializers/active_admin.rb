afc90dc16e970b0fcd35bed284b6c48939375e52
Vipul A M <vipulnsward@gmail.com>
2014-11-18 01:04:48 +0530

    - Moved active_admin css to common styles. - Added active_admin config to load css. - Added assets config to precompile css

--- config/initializers/active_admin.rb
+++ config/initializers/active_admin.rb
@@ -157,7 +157,7 @@ ActiveAdmin.setup do |config|
   # and feel.
   #
   # To load a stylesheet:
-  #   config.register_stylesheet 'my_stylesheet.css'
+  config.register_stylesheet 'common/active_admin'
   #
   # You can provide an options hash for more control, which is passed along to stylesheet_link_tag():
   #   config.register_stylesheet 'my_print_stylesheet.css', :media => :print
