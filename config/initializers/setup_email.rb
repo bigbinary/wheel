fe87fa8dcb6041c85bb6874b25605b513bfea0a5
Neeraj Singh <neerajdotname@gmail.com>
2015-01-20 14:23:51 -0500

    use mail_interceptor

--- config/initializers/setup_email.rb
+++ config/initializers/setup_email.rb
@@ -5,3 +5,8 @@ ActionMailer::Base.delivery_method = Settings.mailer.delivery_method
 if Settings.mailer.delivery_method == :smtp
   ActionMailer::Base.smtp_settings = Settings.mailer.smtp_settings
 end
+
+interceptor = MailInterceptor::Interceptor.new({ forward_emails_to: Settings.intercept_and_forward_emails_to,
+                                                 deliver_emails_to: ["@company.com"],
+                                                 subject_prefix: Settings.subject_prefix_for_outgoing_emails })
+ActionMailer::Base.register_interceptor(interceptor)
