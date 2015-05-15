6b0e9efa498317cc27c7465a4da86952b335b2f8
Vipul A M <vipulnsward@gmail.com>
2015-05-15 16:14:38 +0530

    - Removed handy - Moved settings.yml to secrets.yml - Added defaults and inheritance of the same - Fixed all places using Settings to use Rails.application.secrets
    - Added back handy to allow using tests for services, etc

--- app/workers/base_worker.rb
+++ app/workers/base_worker.rb
@@ -1,7 +1,7 @@
 class BaseWorker
 
   def perform
-    Honeybadger.context(job_name: self.class.name, app_name: Settings.app_name)
+    Honeybadger.context(job_name: self.class.name, app_name: Rails.application.engine_name)
   end
 
 end
