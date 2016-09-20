f6046179200bf0598bff172bcc2be58fc9d91b0b
midhun k <kr1shn4.m1dhun@gmail.com>
2016-09-20 21:55:49 +0530

    126 replace honeybadger with Rollbar (#128)
    Removed method :set_honeybadger_context
     -- Rollbar by default sends request uuid, current_user name and email, more can be configured
        at initializers/rollbar.rb
    
    Removed initializers/honeybadger.rb which sends job name and host
     -- Rollbar sends job data along with job name and method.
    
    Updated README

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,8 +3,6 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
-
-  before_action :set_honeybadger_context
   before_action :set_layout_carrier
 
   private
@@ -15,12 +13,6 @@ class ApplicationController < ActionController::Base
     unless current_user.super_admin?
       redirect_to root_path, status: :forbidden, alert: "Unauthorized Access!"
     end
-  end
-
-  def set_honeybadger_context
-    hash = { uuid: request.uuid }
-    hash.merge!(user_id: current_user.id, user_email: current_user.email) if current_user
-    Honeybadger.context hash
   end
 
   def set_layout_carrier
