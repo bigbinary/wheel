9b2b998268f812eeb52f873257073e4e176df544
Rohit Kumar <rohitz1990@gmail.com>
2016-11-16 18:52:03 +0530

    COMMENT: only rack timeout logger is disabled

--- config/initializers/rack_timeout.rb
+++ config/initializers/rack_timeout.rb
@@ -1,2 +1,5 @@
+# Following code is disabling Rack Timeout logger.
+# Rack Timeout is enabled in development.
 Rack::Timeout::Logger.disable if Rails.env.development?
+
 Rack::Timeout.service_timeout = 30 # seconds
