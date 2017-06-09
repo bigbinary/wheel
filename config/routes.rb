cc639b4069b9881bd57e298f5dfcfe40b892e766
Neeraj Singh <neeraj@bigbinary.com>
2017-06-09 08:13:09 -0400

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,68 @@
-Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+Wheel::Application.routes.draw do
+  # Setting path_prefix makes sure that devise routes do not conflict
+  # with users resources routes.
+  #
+  # More details available here:
+  # https://github.com/plataformatec/devise/wiki/How-To:-Manage-users-through-a-CRUD-interface
+  devise_for :users, path_prefix: 'devise', controllers: { registrations: 'registrations' }
+
+  # Authentication
+  devise_scope :user do
+    get '/login' => 'devise/sessions#new', as: :login
+    get '/logout' => 'sessions#destroy', :as => :logout
+    get '/signup' => 'registrations#new', :as => :signup
+    scope 'my' do
+      get 'profile', to: 'registrations#edit'
+      put 'profile/update', to: 'registrations#update'
+      get 'password/edit', to: 'registrations#edit_password'
+      put 'password/update', to: 'registrations#update_password'
+    end
+  end
+
+  # The priority is based upon order of creation: first created -> highest priority.
+  # See how all your routes lay out with 'rake routes'.
+
+  unauthenticated do
+    get '/logout' => redirect('/')
+  end
+
+  authenticate :user, ->(u) { u.super_admin? } do
+    get '/delayed_job' => DelayedJobWeb, :anchor => false
+    put '/delayed_job' => DelayedJobWeb, :anchor => false
+    post '/delayed_job' => DelayedJobWeb, :anchor => false
+
+    ActiveAdmin.routes(self)
+    namespace :superadmin do
+      root to: 'users#index'
+      resources :users
+    end
+
+  end
+
+  get 'pages/about'
+  get 'pages/contact_us'
+  resources :contacts, only: [:create]
+
+  authenticated :user do
+    get '/pages' => 'pages#index', as: :pages
+  end
+
+  unauthenticated do
+    as :user do
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
+    end
+  end
+
+  namespace :api, defaults: { format: :json } do
+    namespace :v1 do
+      devise_scope :user do
+        post 'login' => 'sessions#create', as: 'login'
+      end
+
+      resources :users, only: [:show, :create, :update, :destroy], constraints: { id: /.*/ }
+    end
+  end
+
+
+  root 'home#index'
 end
