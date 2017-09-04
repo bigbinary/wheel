1c1d29ea491d79aad10c97e1b44491efb3d2b1df
Sharang Dashputre <sharang.d@gmail.com>
2017-09-04 20:50:47 +0530

    Run prettier on all JS and SCSS code

--- app/assets/javascripts/common/bootstrap.js
+++ app/assets/javascripts/common/bootstrap.js
@@ -1,7 +1,5 @@
 $(function() {
-
   $("a[rel~=popover], .has-popover").popover();
 
   return $("a[rel~=tooltip], .has-tooltip").tooltip();
-
 });
--- app/assets/javascripts/common/modal_behavior.js
+++ app/assets/javascripts/common/modal_behavior.js
@@ -1,78 +1,86 @@
-if ( Modal !== undefined ) {
-    alert('Modal is already defined');
+if (Modal !== undefined) {
+  alert("Modal is already defined");
 }
 
 var Modal = (function() {
+  var modalContentSelector = "[data-behavior ~= modal-content]",
+    modalContainerSelector = "[data-behavior ~= modal-container]";
 
-    var modalContentSelector = "[data-behavior ~= modal-content]",
-        modalContainerSelector = "[data-behavior ~= modal-container]";
+  function _displayModal(link) {
+    var $modalContainer = _findOrCreateModalContainer(),
+      url = link.data("url");
 
-    function _displayModal(link) {
-        var $modalContainer = _findOrCreateModalContainer(),
-            url = link.data('url');
+    $modalContainer.load(url, function() {
+      var $element = $(this).find(modalContentSelector);
+      $element.show();
+      $element.find("[data-focus~=true]").focus();
+    });
+  }
 
-        $modalContainer.load(url, function() {
-            var $element = $(this).find(modalContentSelector);
-            $element.show();
-            $element.find('[data-focus~=true]').focus();
-        });
+  function _findOrCreateModalContainer() {
+    var element;
+
+    if ($(modalContainerSelector).length) {
+      element = $(modalContainerSelector);
+    } else {
+      element = $('<div data-behavior="modal-container"></div>');
+      $("body").append(element);
     }
 
-    function _findOrCreateModalContainer() {
-        var element;
+    return element;
+  }
 
-        if ($(modalContainerSelector).length) {
-            element = $(modalContainerSelector);
-        } else {
-            element = $('<div data-behavior="modal-container"></div>');
-            $('body').append(element);
-        }
+  function _formSubmissionResponseHandler(data) {
+    var modalContainer = _findOrCreateModalContainer();
 
-        return element
-    };
-
-    function _formSubmissionResponseHandler(data) {
-        var modalContainer = _findOrCreateModalContainer();
-
-        if (data.modal_content) {
-            modalContainer.html(data.modal_content)
-            modalContainer.find(modalContentSelector).show();
-
-        } else if (data.redirect_to) {
-            window.location.href = data.redirect_to;
-
-        } else {
-            modalContainer.find(modalContentSelector).hide();
-        };
+    if (data.modal_content) {
+      modalContainer.html(data.modal_content);
+      modalContainer.find(modalContentSelector).show();
+    } else if (data.redirect_to) {
+      window.location.href = data.redirect_to;
+    } else {
+      modalContainer.find(modalContentSelector).hide();
     }
+  }
 
-    function displayInModal(event) {
-        event.preventDefault();
-        _displayModal($(this));
-    };
+  function displayInModal(event) {
+    event.preventDefault();
+    _displayModal($(this));
+  }
 
-    function hide(event) {
-        event.preventDefault()
-        $(this).closest(modalContentSelector).hide();
-    };
+  function hide(event) {
+    event.preventDefault();
+    $(this)
+      .closest(modalContentSelector)
+      .hide();
+  }
 
-    function submitForm(event){
-        var form = $(this);
-        event.preventDefault();
+  function submitForm(event) {
+    var form = $(this);
+    event.preventDefault();
 
-        $.ajax({
-            type: form.attr('method'),
-            url: form.attr('action'),
-            data: form.serialize()
-        }).done(_formSubmissionResponseHandler);
-    };
+    $.ajax({
+      type: form.attr("method"),
+      url: form.attr("action"),
+      data: form.serialize(),
+    }).done(_formSubmissionResponseHandler);
+  }
 
-    return { "displayInModal": displayInModal,
-             "hide": hide,
-             "submitForm": submitForm }
-
+  return {
+    displayInModal: displayInModal,
+    hide: hide,
+    submitForm: submitForm,
+  };
 })();
 
-$(document).on('click', 'a[data-behavior ~= display-in-modal]', Modal.displayInModal);
-$(document).on("click", "[data-behavior ~= modal-close]", Modal.hide)
-$(document).on('submit', "[data-behavior ~= modal-container] form", Modal.submitForm)
+$(document).on(
+  "click",
+  "a[data-behavior ~= display-in-modal]",
+  Modal.displayInModal
+);
+$(document).on("click", "[data-behavior ~= modal-close]", Modal.hide);
+$(document).on(
+  "submit",
+  "[data-behavior ~= modal-container] form",
+  Modal.submitForm
+);
