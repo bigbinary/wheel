e29a425240568233949ec1c68fc9e408fe3fac5a
Neeraj Singh <neeraj@bigbinary.com>
2018-02-27 15:21:21 -0800

    Scaffold should generate js files and not coffeescript files

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,9 @@ module Wheel
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    #
+    # scaffold should generate js files and not coffeescript files
+    config.generators.javascript_engine :js
+
   end
 end
