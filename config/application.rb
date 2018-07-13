95cf8fe207d338a57c4f6bad840f06a4629e5233
Neeraj Singh <neeraj@bigbinary.com>
2018-07-14 02:08:56 +0530

    WIP : Rails upgrade 5 2 round 2 (#193)
    * Update to Rails 5.2
    
    * Revert routes
    
    * Add missing bootsnap
    
    * Build up credentials file with sample master key
    
    * adding Gemfil.lock
    
    * fix merge conflict
    
    * comment listen gem
    
    * deleted credentials.yml.enc
    
    * fix running migration

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,6 @@
-# frozen_string_literal: true
+require_relative 'boot'
 
-require_relative "boot"
-
-require "rails/all"
+require 'rails/all'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -14,11 +12,8 @@ module Wheel
     config.load_defaults 5.1
 
     # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
-    #
-    # scaffold should generate js files and not coffeescript files
-    config.generators.javascript_engine :js
-
+    # Application configuration can go into files in config/initializers
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and any gems in your application.
   end
 end
