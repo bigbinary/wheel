8b3279a913908e388ba7028a6cb96fffc796e890
Neeraj Singh <neeraj@bigbinary.com>
2018-07-14 01:23:25 +0530

    Remove carrierwave as preparation for upgrade to Rails 5.2 (#191)
    * remove code related to file upload in prep for Rails 5.2
    
    * remove profile_image feature

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140225143027) do
+ActiveRecord::Schema.define(version: 20180713193251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -61,7 +61,6 @@ ActiveRecord::Schema.define(version: 20140225143027) do
     t.string "first_name"
     t.string "role", default: "standard"
     t.string "authentication_token"
-    t.string "profile_image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
