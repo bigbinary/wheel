1e3397b7130fa3624eefe16c002768675f5e606d
hari816 <harinath816@gmail.com>
2019-09-18 12:11:50 +0530

    removed-sidekiq-worker-test

--- test/workers/send_msg_of_user_worker_test.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-class SendMsgOfUserWorkerTest < Minitest::Test
-  def test_example
-    skip "add some examples to (or delete) #{__FILE__}"
-  end
-end
