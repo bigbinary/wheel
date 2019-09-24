27556784203e23083aa55c6f14046a4127deef61
hari816 <harinath816@gmail.com>
2019-09-24 19:45:17 +0530

    removed deprecation warning Module#parent_name

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,7 @@ module Wheel
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
     config.active_job.queue_adapter = :sidekiq
+    config.action_dispatch.return_only_media_type_on_content_type = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
--- config/environment.rb
+++ config/environment.rb
@@ -2,4 +2,4 @@
 require_relative 'application'
 
 # Initialize the Rails application.
-Rails.application.initialize!
+ Rails.application.class.module_parent_name
