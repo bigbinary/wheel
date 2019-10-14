bcf785247fb7b756cc692d7b09fe7b4747a66c56
Rohan Raghavendra <46399838+Rohann7@users.noreply.github.com>
2019-10-15 02:21:18 +0530

    Upgraded Ruby to 2.6.5 (#258)
    * Changed default ids to uuid
    
    * Upgraded Ruby to 2.6.5
    
    * reverting non ruby upgrade changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_09_16_121842) do
+ActiveRecord::Schema.define(version: 2018_07_13_193251) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
