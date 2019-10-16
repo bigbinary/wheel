83e9cd1cf81229f5eac0b12c2bb56ec4a08d75a4
Rohann7 <raghavendra.bandaru@bigbinary.com>
2019-10-16 20:03:52 +0530

    Removed all code related to docker and updated README

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-cd /wheel
-bundle install
-cp config/database.yml.docker config/database.yml
-bundle exec rake setup
-rm tmp/pids/server.pid || true
-bundle exec rails server -p 9000 -b 0.0.0.0
