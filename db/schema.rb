2067c2c8455feb82a6c2ded92ce3d18d895805da
Rohan Raghavendra <46399838+Rohann7@users.noreply.github.com>
2019-10-17 20:29:41 +0530

    Changed default ids to uuid (#261)

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_07_13_193251) do
+ActiveRecord::Schema.define(version: 2019_10_17_133002) do
 
   # These are extensions that must be enabled in order to support this database
+  enable_extension "pgcrypto"
   enable_extension "plpgsql"
 
   create_table "active_admin_comments", force: :cascade do |t|
