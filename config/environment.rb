5e4b906a1b714946eddabdda4115c0959b40c466
Ashik Salman <ashiksp@gmail.com>
2020-03-14 02:36:22 +0530

    Environment config updates (#291)
    * Updated sample database.yml file.
    
    * Added sidekiq config file.
    
    * Removed rails_12factor gem.
    
    Added required config changes for heroku logging.
    
    * Removed tagged_logging initializer file.
    
    The tagged logging configs are handled separately for each env.
    
    * Do not dump schema after migrations.
    
    https://groups.google.com/forum/#!topic/rubyonrails-core/h4cQXmKuB7M
    https://github.com/rails/rails/pull/13948
    
    * Enabled force_ssl config.
    
    * Added comments about config/initializers/assets.rb.
    
    * reverting some of the changes
    
    Co-authored-by: Raj Singh <raj.singh@vineti.com>

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,13 @@
 # Load the Rails application.
 require_relative 'application'
 
+Rails.application.configure do
+  if ENV["RAILS_LOG_TO_STDOUT"].present?
+    logger           = ActiveSupport::Logger.new(STDOUT)
+    logger.formatter = config.log_formatter
+    config.logger    = ActiveSupport::TaggedLogging.new(logger)
+  end
+end
+
 # Initialize the Rails application.
 Rails.application.initialize!
--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -34,9 +34,6 @@ Rails.application.configure do
   # Generate digests for assets URLs.
   config.assets.digest = true
 
-  # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = "1.0"
-
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
@@ -48,7 +45,7 @@ Rails.application.configure do
   config.log_level = :debug
 
   # Prepend all log lines with the following tags.
-  # config.log_tags = [ :subdomain, :uuid ]
+  config.log_tags = [ :request_id ]
 
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
@@ -58,10 +55,6 @@ Rails.application.configure do
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = "http://assets.example.com"
-
-  # Precompile additional assets.
-  # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-  # config.assets.precompile += %w( search.js )
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
