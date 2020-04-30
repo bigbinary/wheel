08372c55ca9c01f3532709c85bbf752b2f626e4f
Ershad Kunnakkadan <ershad92@gmail.com>
2020-05-01 03:42:22 +0530

    Enable zeitwerk for autoloading (#301)

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module Wheel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults "6.0" # Enable zeitwerk mode in CRuby
     config.active_job.queue_adapter = :sidekiq
     config.action_dispatch.return_only_media_type_on_content_type = false
 
