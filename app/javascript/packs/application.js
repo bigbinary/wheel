3fa9d32d43d707ff446a1e0f62524f3bb6be87bf
Farhad <farhad9801@gmail.com>
2020-05-23 04:16:28 +0400

    migrate sprockets to webpacker, add missing assests for active admin

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import "bootstrap";
+import "@activeadmin/activeadmin";
+
 console.log("Hello World from Webpacker");
