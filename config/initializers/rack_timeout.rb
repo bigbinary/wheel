bb018d518b636c7005cebc67bc4c7628565073a0
Neeraj Singh <neeraj@bigbinary.com>
2020-07-09 11:52:06 -0700

    remove rack-timeout

--- config/initializers/rack_timeout.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-# Following code is disabling Rack Timeout logger.
-# Rack Timeout is enabled in development.
-Rack::Timeout::Logger.disable if Rails.env.development?
-
-# Rack::Timeout.service_timeout = 30 # seconds
