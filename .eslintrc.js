b92d3b68c535f1136f13725feeda34c0e985cdf5
Amal Jose A <amal.jose@bigbinary.com>
2020-07-16 19:16:50 +0530

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,9 @@
 module.exports = {
   "env": {
     "browser": true,
-    "es6": true
+    "es6": true,
+    "node": true,
+    "amd": true
   },
   "extends": ["eslint:recommended", "plugin:react/recommended", "prettier"],
   "settings": {
