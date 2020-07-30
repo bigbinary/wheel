751f20c7b8eca25aa02a8f9c9f79243db188e94d
Rishi Mohan <iamrishi.ms@gmail.com>
2020-07-30 23:15:38 +0530

    Added tailwind

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,4 +7,5 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
+import("../stylesheets/application.scss");
 console.log("Hello World from Webpacker");
--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,7 @@
 module.exports = {
   plugins: [
     require('postcss-import'),
+    require('tailwindcss')('./tailwind.config.js'),
     require('postcss-flexbugs-fixes'),
     require('postcss-preset-env')({
       autoprefixer: {
