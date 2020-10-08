e72a2426177717be4377fc51974aab5c3ba56a70
Shivam Chahar <chahar.shivam@yahoo.com>
2020-10-08 12:15:13 +0530

    Added aliases for react

---
+++ config/webpack/custom.js
@@ -0,0 +1,10 @@
+module.exports = {
+  resolve: {
+    alias: {
+      apis: "src/apis",
+      components: "src/components",
+      contexts: "src/contexts",
+      reducers: "src/reducers",
+    },
+  },
+};
