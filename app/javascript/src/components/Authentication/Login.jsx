e6c8cec746a39dec05b94ba17dd70b55c29816db
Vinay Chandran <vinaychandranvs@gmail.com>
2020-10-11 23:37:29 +0530

    Use NitroUI components in login and signup page

--- app/javascript/src/components/Authentication/Login.jsx
+++ app/javascript/src/components/Authentication/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Button, Input } from "nitroui";
 import { Toastr } from "common";
 
 import { login } from "apis/authentication";
@@ -45,61 +46,32 @@ const Login = ({ history }) => {
             className="w-full px-10 py-8 bg-white border rounded-lg shadow-sm simple_form"
             onSubmit={handleSubmit}
           >
-            <div className="mb-8 form-group email required user_email">
-              <div>
-                <label
-                  className="block mb-1 text-sm font-medium text-gray-600 email required control-label tracking"
-                  htmlFor="user_email"
-                >
-                  Email
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="w-full px-3 py-2 text-gray-800 transition duration-200 ease-in-out border border-gray-400 rounded-md string email required form-control focus:text-black / hover:border-gray-600 focus:border-gray-600 focus:outline-none"
-                  autoFocus
-                  required
-                  aria-required="true"
-                  type="email"
-                  value={email}
-                  name="email"
-                  id="user_email"
-                  onChange={e => setEmail(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group email required user_email">
+              <Input
+                required
+                type="email"
+                value={email}
+                label="Email"
+                placeholder="sam@example.com"
+                id="user_email"
+                onChange={e => setEmail(e.target.value)}
+              />
             </div>
 
-            <div className="mb-8 form-group password required user_password">
-              <div>
-                <label
-                  className="block mb-1 text-sm font-medium text-gray-600 password required control-label tracking"
-                  htmlFor="user_password"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="w-full px-3 py-2 text-gray-800 transition duration-200 ease-in-out border border-gray-400 rounded-md password required form-control focus:text-black / hover:border-gray-600 focus:border-gray-600 focus:outline-none"
-                  required
-                  aria-required="true"
-                  type="password"
-                  name="password"
-                  id="user_password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group password required user_password">
+              <Input
+                type="password"
+                name="password"
+                id="user_password"
+                label="Password"
+                placeholder="******"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
             </div>
 
             <div className="flex items-center justify-between">
-              <input
-                type="submit"
-                name="commit"
-                value={loading ? "Loading..." : "Login"}
-                className="w-full px-4 py-2 text-base font-semibold text-white transition duration-200 ease-in-out bg-teal-600 border border-teal-600 rounded-md cursor-pointer btn / hover:opacity-75"
-                data-disable-with="Login"
-              />
+              <Button type="submit" loading={loading} label="Login" />
             </div>
           </form>
           <div className="mt-2 text-center">
--- app/javascript/src/components/Authentication/Signup.jsx
+++ app/javascript/src/components/Authentication/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { Button, Input } from "nitroui";
 
 import { signup } from "apis/users";
 import { useAuthDispatch } from "contexts/auth";
@@ -46,144 +47,90 @@ const Signup = ({ history }) => {
   };
 
   return (
-    <div className="wrapper flex flex-grow">
-      <div className="flex-col container mx-auto px-4">
-        <div className="flex flex-col flex-grow h-full w-full items-center justify-center lg:w-5/12 mx-auto py-20">
-          <h2 className="text-2xl text-center font-medium mb-5 text-gray-800">
+    <div className="flex flex-grow wrapper">
+      <div className="container flex-col px-4 mx-auto">
+        <div className="flex flex-col items-center justify-center flex-grow w-full h-full py-20 mx-auto lg:w-5/12">
+          <h2 className="mb-5 text-2xl font-medium text-center text-gray-800">
             Signup
           </h2>
 
           <form
-            className="simple_form bg-white border shadow-sm rounded-lg px-10 py-8 w-full"
+            className="w-full px-10 py-8 bg-white border rounded-lg shadow-sm simple_form"
             onSubmit={handleSubmit}
           >
-            <div className="form-group mb-8 email required user_email">
-              <div>
-                <label
-                  className="email required control-label block mb-1 font-medium text-gray-600 text-sm tracking"
-                  htmlFor="user_email"
-                >
-                  Email
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="string email required form-control border border-gray-400 text-gray-800 focus:text-black / w-full px-3 py-2 rounded-md hover:border-gray-600 focus:border-gray-600 focus:outline-none transition duration-200 ease-in-out"
-                  autoFocus
-                  required
-                  aria-required="true"
-                  type="email"
-                  value={email}
-                  onChange={e => setEmail(e.target.value)}
-                  name="email"
-                  id="user_email"
-                />
-              </div>
+            <div className="mb-4 form-group email required user_email">
+              <Input
+                required
+                aria-required="true"
+                type="email"
+                label="Email"
+                placeholder="sam@example.com"
+                value={email}
+                onChange={e => setEmail(e.target.value)}
+                name="email"
+                id="user_email"
+              />
             </div>
 
-            <div className="form-group mb-8 string required user_first_name">
-              <div>
-                <label
-                  className="string required control-label block mb-1 font-medium text-gray-600 text-sm tracking"
-                  htmlFor="user_first_name"
-                >
-                  First name
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="string required form-control border border-gray-400 text-gray-800 focus:text-black / w-full px-3 py-2 rounded-md / hover:border-gray-600 focus:border-gray-600 focus:outline-none / transition duration-200 ease-in-out"
-                  required
-                  aria-required="true"
-                  type="text"
-                  name="first_name"
-                  id="user_first_name"
-                  value={firstName}
-                  onChange={e => setFirstName(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group string required user_first_name">
+              <Input
+                required
+                aria-required="true"
+                type="text"
+                label="First name"
+                placeholder="Sam"
+                name="first_name"
+                id="user_first_name"
+                value={firstName}
+                onChange={e => setFirstName(e.target.value)}
+              />
             </div>
 
-            <div className="form-group mb-8 string required user_last_name">
-              <div>
-                <label
-                  className="string required control-label block mb-1 font-medium text-gray-600 text-sm tracking"
-                  htmlFor="user_last_name"
-                >
-                  Last name
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="string required form-control border border-gray-400 text-gray-800 focus:text-black / w-full px-3 py-2 rounded-md / hover:border-gray-600 focus:border-gray-600 focus:outline-none / transition duration-200 ease-in-out"
-                  required
-                  aria-required="true"
-                  type="text"
-                  name="last_name"
-                  id="user_last_name"
-                  value={lastName}
-                  onChange={e => setLastName(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group string required user_last_name">
+              <Input
+                required
+                aria-required="true"
+                type="text"
+                name="last_name"
+                placeholder="Smith"
+                label="Last name"
+                id="user_last_name"
+                value={lastName}
+                onChange={e => setLastName(e.target.value)}
+              />
             </div>
 
-            <div className="form-group mb-8 password required user_password">
-              <div>
-                <label
-                  className="password required control-label block mb-1 font-medium text-gray-600 text-sm tracking"
-                  htmlFor="user_password"
-                >
-                  Password
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="password required form-control border border-gray-400 text-gray-800 focus:text-black / w-full px-3 py-2 rounded-md / hover:border-gray-600 focus:border-gray-600 focus:outline-none / transition duration-200 ease-in-out"
-                  required
-                  aria-required="true"
-                  type="password"
-                  name="password"
-                  id="user_password"
-                  value={password}
-                  onChange={e => setPassword(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group password required user_password">
+              <Input
+                required
+                type="password"
+                name="password"
+                label="Password"
+                placeholder="******"
+                id="user_password"
+                value={password}
+                onChange={e => setPassword(e.target.value)}
+              />
             </div>
 
-            <div className="form-group mb-8 password required user_password_confirmation">
-              <div>
-                <label
-                  className="password required control-label block mb-1 font-medium text-gray-600 text-sm tracking"
-                  htmlFor="user_password_confirmation"
-                >
-                  Password confirmation
-                </label>
-              </div>
-              <div className="controls">
-                <input
-                  className="password required form-control border border-gray-400 text-gray-800 focus:text-black / w-full px-3 py-2 rounded-md / hover:border-gray-600 focus:border-gray-600 focus:outline-none / transition duration-200 ease-in-out"
-                  required
-                  aria-required="true"
-                  type="password"
-                  name="password_confirmation"
-                  id="user_password_confirmation"
-                  value={passwordConfirmation}
-                  onChange={e => setPasswordConfirmation(e.target.value)}
-                />
-              </div>
+            <div className="mb-4 form-group password required user_password_confirmation">
+              <Input
+                required
+                type="password"
+                label="Confirm password"
+                placeholder="******"
+                name="password_confirmation"
+                id="user_password_confirmation"
+                value={passwordConfirmation}
+                onChange={e => setPasswordConfirmation(e.target.value)}
+              />
             </div>
 
-            <input
-              type="submit"
-              name="commit"
-              value={loading ? "Loading..." : "Get started"}
-              className="btn btn font-semibold text-base text-white / px-4 py-2 w-full rounded-md / bg-teal-600 border border-teal-600 / cursor-pointer / hover:opacity-75 / transition duration-200 ease-in-out"
-              data-disable-with="Get started"
-            />
+            <Button type="submit" loading={loading} label="Signup" />
           </form>
           <div className="mt-2 text-center">
             <Link
-              className="block text-teal-600 hover:text-black mt-2"
+              className="block mt-2 text-teal-600 hover:text-black"
               to="/login"
             >
               Sign in instead
