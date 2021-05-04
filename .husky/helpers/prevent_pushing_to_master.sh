e2e191a4255b00752629d5aa0acf739e0015ee05
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2021-05-04 21:51:38 +0530

    Allow pushing to heroku (#487)

--- .husky/helpers/prevent_pushing_to_master.sh
+++ .husky/helpers/prevent_pushing_to_master.sh
@@ -2,9 +2,10 @@
 
 prevent_pushing_to_master() {
   current_branch=`git symbolic-ref HEAD`
-  if [ "$current_branch" = "refs/heads/master" -o "$current_branch" = "refs/heads/main" ]
+  current_origin=`git remote`
+  if [ current_origin = "origin" -o "$current_branch" = "refs/heads/master" -o "$current_branch" = "refs/heads/main" ]
   then
-    echo "You are not authorized to push directly to master/main branch. Push from a new branch and make the PR.";
+    echo "You are not authorized to push/commit directly to master/main branch in origin remote. Push from a new branch and make the PR.";
     exit 1;
   fi
 }
