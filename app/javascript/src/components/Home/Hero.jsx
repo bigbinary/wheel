f9c0d25e648787f57b7b17b87dcccdcc352463d3
Goutham Subramanyam <gsn.goutham@gmail.com>
2021-05-21 21:06:54 +0530

    Fixed scroll issue in Hero screen

--- app/javascript/src/components/Home/Hero.jsx
+++ app/javascript/src/components/Home/Hero.jsx
@@ -17,15 +17,17 @@ export default function Hero() {
   ];
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen max-w-3xl p-8 m-auto">
-      <h1 className="mb-3 text-4xl font-bold">Wheel</h1>
-      <p className="mx-auto mb-6 text-lg text-center text-gray-800">
-        Don&apos;t reinvent the wheel. Use sane defaults to bootstrap your
-        react-rails project!
-      </p>
-      <Features features={features} />
-      <div className="flex items-center justify-center mt-6">
-        <Button size="large" type="primary" to="/login" label="Login" />
+    <div className="flex flex-row items-center justify-center h-screen">
+      <div className="flex flex-col items-center justify-center max-w-3xl p-8 m-auto">
+        <h1 className="mb-3 text-4xl font-bold">Wheel</h1>
+        <p className="mx-auto mb-6 text-lg text-center text-gray-800">
+          Don&apos;t reinvent the wheel. Use sane defaults to bootstrap your
+          react-rails project!
+        </p>
+        <Features features={features} />
+        <div className="flex items-center justify-center mt-6">
+          <Button size="large" type="primary" to="/login" label="Login" />
+        </div>
       </div>
     </div>
   );
@@ -35,7 +37,7 @@ const Features = ({ features }) => {
   return (
     <ul className="list-disc">
       {features.map((feature, index) => (
-        <li key={index} className="py-4 border-b border-gray-100 last:border-0">
+        <li key={index} className="py-3 border-b border-gray-100 last:border-0">
           {feature}
         </li>
       ))}
