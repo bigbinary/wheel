da300be5bfe026391f37c8cf9ceaa43d37c54d75
Chirag Shah <chirag@bigbinary.com>
2021-05-28 17:37:21 +0530

    Updated reset password form to use formik

--- app/javascript/src/components/Authentication/ResetPassword.jsx
+++ app/javascript/src/components/Authentication/ResetPassword.jsx
@@ -1,8 +1,12 @@
-import React, { useState } from "react";
-import { Button, Input } from "neetoui";
+import React from "react";
+import { Form, Formik } from "formik";
+import { Input as FormikInput } from "neetoui/formik";
+import { Button } from "neetoui";
 
 const ResetPassword = () => {
-  const [email, setEmail] = useState("");
+  const handleSubmit = () => {
+    // trigger api call for reset password
+  };
 
   return (
     <div className="flex flex-row items-center justify-center w-screen h-screen p-6 bg-gray-100 overflow-y-auto overflow-x-hidden">
@@ -14,36 +18,31 @@ const ResetPassword = () => {
           Enter your email address below and we&apos;ll send you a link to reset
           your password.
         </div>
-        <form className="w-full p-8 space-y-6 bg-white border rounded-md shadow" id="new_user">
-          <Input
-            id="user_email"
-            label="Email"
-            type="email"
-            required
-            value={email}
-            onChange={e => setEmail(e.target.value)}
-          />
-          <div className="flex flex-col items-center justify-center space-y-2">
-            <Button
-              type="submit"
-              label="Send reset password email"
-              data-disable-with="Send reset password email"
-              fullWidth
-            />
-            <Button
-              label="Back"
-              style="link"
-              to="/login"
-            />
-          </div>
-        </form>
+        <Formik
+          initialValues={{
+            email: "",
+          }}
+          onSubmit={handleSubmit}
+        >
+          <Form
+            className="w-full p-8 space-y-6 bg-white border rounded-md shadow"
+            id="new_user"
+          >
+            <FormikInput name="email" label="Email" type="email" required />
+            <div className="flex flex-col items-center justify-center space-y-2">
+              <Button
+                type="submit"
+                label="Send reset password email"
+                data-disable-with="Send reset password email"
+                fullWidth
+              />
+              <Button label="Back" style="link" to="/login" />
+            </div>
+          </Form>
+        </Formik>
         <div className="flex flex-row items-center justify-start mt-4 space-x-1">
-          <p className="font-normal text-gray-600">Don't have an account?</p>
-          <Button
-            label="Signup"
-            style="link"
-            to="/signup"
-          />
+          <p className="font-normal text-gray-600">{`Don't have an account?`}</p>
+          <Button label="Signup" style="link" to="/signup" />
         </div>
       </div>
     </div>
