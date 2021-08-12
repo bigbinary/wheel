2f28679cb1f43e694aafabf9600f9f0f5bbb8ac1
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2021-08-12 21:45:16 +0530

    Changed commands for docker app setup and added database.yml.docker file

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,9 +8,7 @@ rm -f $APP_PATH/tmp/pids/server.pid
 
 database_config_path="$APP_PATH/config/database.yml"
 
-if [ ! -f "$database_config_path" ]; then
-  rm database_config_path
-fi
+rm -f database_config_path
 cp $APP_PATH/config/database.yml.docker "$database_config_path"
 
 # append bundle exec to every command
