982bbc07f8dc7e5c82896c5dba380c3216a623d5
Neeraj Singh <neeraj@bigbinary.com>
2021-08-12 04:24:52 -0400

    cleaned up sidekiq initializer and routes

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,20 +1,27 @@
 # frozen_string_literal: true
 
+Redis.exists_returns_integer = true
+
 if Rails.env.heroku?
-  require 'sidekiq/testing'
+  require "sidekiq/testing"
   Sidekiq::Testing.inline!
 end
 
+if Rails.env.test?
+  Sidekiq.logger.level = Logger::WARN
+end
+
+Sidekiq::Extensions.enable_delay!
+
 Sidekiq.configure_server do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'] || 'redis',
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { url: Rails.application.secrets.redis_url, size: 9 }
+  schedule_file = Rails.root.join("config/schedule.yml")
+
+  if File.exists?(schedule_file)
+    Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
+  end
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    host: ENV['REDIS_HOST'] || 'redis',
-    port: ENV['REDIS_PORT'] || '6379'
-  }
+  config.redis = { url: Rails.application.secrets.redis_url, size: 1 }
 end
