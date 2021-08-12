5431bd37304be4a6a8d012424fe32dfe552e6446
Ghouse Mohamed <ghousemohamed7126@gmail.com>
2021-08-12 18:14:28 +0530

    Added changes for setting up database.yml automatically during docker setup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -9,8 +9,9 @@ rm -f $APP_PATH/tmp/pids/server.pid
 database_config_path="$APP_PATH/config/database.yml"
 
 if [ ! -f "$database_config_path" ]; then
-  cp $APP_PATH/config/database.yml.postgresql "$database_config_path"
+  rm database_config_path
 fi
+cp $APP_PATH/config/database.yml.docker "$database_config_path"
 
 # append bundle exec to every command
 bundle exec ${@}
