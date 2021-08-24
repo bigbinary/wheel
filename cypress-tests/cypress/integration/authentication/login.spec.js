e7c1a662b36b3f68163c6a6984a3f8a08b934a3c
prasanna m s <prasannams0420@gmail.com>
2021-08-24 19:29:08 +0530

    add login tests

---
+++ cypress-tests/cypress/integration/authentication/login.spec.js
@@ -0,0 +1,53 @@
+import { fake } from "Fixtures/fake";
+import { loginSelectors } from "Selectors/login";
+import { loginTexts } from "Texts/login";
+import { loginPath, notesPath } from "Constants/routes";
+
+describe("Login", () => {
+  let user;
+  const invalidCredential = fake.word;
+
+  before(() => {
+    cy.fixture("credentials/oliver.json").then(oliver => {
+      user = oliver;
+    });
+  });
+
+  beforeEach(() => {
+    cy.visit(loginPath);
+
+    cy.get(loginSelectors.forgotPasswordLink).should("exist");
+    cy.get(loginSelectors.signUpLink).should("exist");
+  });
+
+  it("should not be able to login with invalid credentials", () => {
+    cy.get(loginSelectors.emailTextField).clear();
+    cy.get(loginSelectors.passwordTextField).clear();
+    cy.get(loginSelectors.submitButton).click();
+
+    cy.get(loginSelectors.inputErrorMessage).should(
+      "contain.text",
+      loginTexts.required
+    );
+
+    cy.clearAndType(loginSelectors.emailTextField, invalidCredential);
+    cy.clearAndType(loginSelectors.passwordTextField, user.password);
+    cy.get(loginSelectors.submitButton).click();
+
+    cy.get(loginSelectors.inputErrorMessage).should(
+      "have.inSensTrimmedText",
+      loginTexts.invalidEmailMessage
+    );
+
+    cy.clearAndType(loginSelectors.emailTextField, user.email);
+    cy.clearAndType(loginSelectors.passwordTextField, invalidCredential);
+    cy.get(loginSelectors.submitButton).click();
+    cy.verifyToastMessage(loginTexts.invalidPasswordMessage);
+  });
+
+  it("should be able to login with valid credentials", () => {
+    cy.loginViaUI(user.email, user.password);
+    cy.verifyToastMessage(loginTexts.loginMessage);
+    cy.url().should("include", notesPath);
+  });
+});
