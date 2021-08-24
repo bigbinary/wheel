79f5e5d61135fb4577c42a30bffa7112260f065e
prasanna m s <prasannams0420@gmail.com>
2021-08-24 19:28:05 +0530

    add login and common files for selectors and texts

---
+++ cypress-tests/cypress/constants/selectors/common.js
@@ -0,0 +1,6 @@
+import { dataCy } from "Support/utils/common";
+
+export const commonSelectors = {
+  toastMessage: dataCy("toastr-message-container"),
+  toastCloseButton: ".Toastify__toast > .Toastify__close-button"
+};
---
+++ cypress-tests/cypress/constants/texts/login.js
@@ -0,0 +1,6 @@
+export const loginTexts = {
+  loginMessage: "Logged in successfully.",
+  required: "Required",
+  invalidEmailMessage: "Invalid email address",
+  invalidPasswordMessage: "Invalid password"
+};
