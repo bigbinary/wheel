74f4b6fb9f9b86beee3709cdaf377d8d6a527d21
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2021-10-04 12:51:28 +0530

    Redirected unshallow error (#651)

--- .semaphore/commands/run_eslint_on_modified_files.sh
+++ .semaphore/commands/run_eslint_on_modified_files.sh
@@ -1,5 +1,5 @@
 checkout
-git fetch --unshallow
+git fetch --unshallow 2> /dev/null
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin master
 git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/master | grep --color=none -i -e "\.js$" -e "\.jsx$" | xargs npx eslint
