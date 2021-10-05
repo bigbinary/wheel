c55359b1868ae72219d5c9d14428bbce5debe454
yedhink <yedhin1998@gmail.com>
2021-10-05 14:18:00 +0530

    Upgraded Ruby to 3.0.2 and Rails to 6.1.4.1

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ module Wheel
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    # config.time_zone = "Central Time (US & Canada)"
+    # config.eager_load_paths << Rails.root.join("extras")
 
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
