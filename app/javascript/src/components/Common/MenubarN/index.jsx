26dd1a8407f2fb37514e590aa753bee8040fe574
Prasidh Prabudhan <prasidhprabudhan@Prasidhs-MacBook-Pro.local>
2021-10-13 13:50:17 +0530

    Added sidebar and menubar

---
+++ app/javascript/src/components/Common/MenubarN/index.jsx
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+
+import { Search, Settings, Plus } from "@bigbinary/neeto-icons";
+import { Typography } from "@bigbinary/neetoui/v2";
+import { MenuBar } from "@bigbinary/neetoui/v2/layouts";
+
+const MenubarN = () => {
+  // const [showMenu, setShowMenu] = useState(true);
+  const [isSearchCollapsed, setIsSearchCollapsed] = useState(true);
+
+  return (
+    <div className="flex">
+      <MenuBar
+        showMenu={true}
+        title={
+          <div className="flex justify-between">
+            <Typography style="h2">Notes</Typography>
+          </div>
+        }
+      >
+        <MenuBar.Block label="All" count={13} active />
+        <MenuBar.Block label="Users" count={2} />
+        <MenuBar.Block label="Leads" count={7} />
+        <MenuBar.Block label="Visitors" count={4} />
+
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Search size={20} />,
+              onClick: () => setIsSearchCollapsed(!isSearchCollapsed)
+            }
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Segments
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Search
+          collapse={isSearchCollapsed}
+          onCollapse={() => setIsSearchCollapsed(true)}
+        />
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+        <MenuBar.SubTitle
+          iconProps={[
+            {
+              icon: () => <Settings size={20} />
+            },
+            {
+              icon: () => <Plus size={20} />
+            },
+            {
+              icon: () => <Search size={20} />
+            }
+          ]}
+        >
+          <Typography
+            component="h4"
+            style="h5"
+            textTransform="uppercase"
+            weight="bold"
+          >
+            Tags
+          </Typography>
+        </MenuBar.SubTitle>
+        <MenuBar.Block label="Europe" count={80} />
+        <MenuBar.Block label="Middle-East" count={60} />
+        <MenuBar.Block label="Asia" count={60} />
+      </MenuBar>
+    </div>
+  );
+};
+export default MenubarN;
--- app/javascript/src/components/Common/Navbar/index.jsx
+++ app/javascript/src/components/Common/Navbar/index.jsx
@@ -1,53 +1,130 @@
 import React from "react";
 
-import { Toastr } from "neetoui";
+// import { Toastr } from "neetoui";
+
+// import authenticationApi from "apis/authentication";
+// import { resetAuthTokens } from "apis/axios";
+//import { useAuthDispatch } from "contexts/auth";
+
+//import AccountDropdown from "./AccountDropdown";
+//import NavItem from "./NavItem";
+
+import { UserCircle } from "@bigbinary/neeto-icons";
+import { Sidebar } from "@bigbinary/neetoui/v2/layouts";
 import { withRouter } from "react-router-dom";
+import { Route, Switch, BrowserRouter } from "react-router-dom";
 
-import authenticationApi from "apis/authentication";
-import { resetAuthTokens } from "apis/axios";
-import { useAuthDispatch } from "contexts/auth";
+import MenubarC from "components/Common/MenubarC";
+import MenubarN from "components/Common/MenubarN";
 
-import AccountDropdown from "./AccountDropdown";
-import NavItem from "./NavItem";
+import icon from "./Group.png";
+import inbox from "./Inbox.jpg";
+//import { none } from "ramda";
 
 const NavBar = () => {
-  const authDispatch = useAuthDispatch();
-  const handleLogout = async () => {
-    try {
-      await authenticationApi.logout();
-      authDispatch({ type: "LOGOUT" });
-      resetAuthTokens();
-      window.location.href = "/";
-    } catch (error) {
-      Toastr.error(error);
-    }
-  };
+  //const authDispatch = useAuthDispatch();
+  // const handleLogout = async () => {
+  //   try {
+  //     await authenticationApi.logout();
+  //     authDispatch({ type: "LOGOUT" });
+  //     resetAuthTokens();
+  //     window.location.href = "/";
+  //   } catch (error) {
+  //     Toastr.error(error);
+  //   }
+  // };
 
   return (
-    <div className="bg-gray-100 nh-sidebar" key="sidebar">
-      <div className="nh-logo">
-        <div className="flex items-center justify-center w-8 h-8 rounded-md">
-          <i className="text-purple-500 ri-flashlight-fill ri-2x" />
+    <BrowserRouter>
+      <div className="flex flex-row items-start justify-start">
+        <Sidebar
+          // organizationInfo={{
+          //   name: 'neetoUI',
+          //   subdomain: 'neetoui.netlify.app'
+          // }}
+          profileInfo={{
+            dropdownProps: [
+              {
+                label: "Edit",
+                onClick: function noRefCheck() {}
+              },
+              {
+                label: "Logout",
+                onClick: function noRefCheck() {}
+              }
+            ],
+            email: "kieranmiller@gmail.com",
+            imageUrl: "https://randomuser.me/api/portraits/women/90.jpg",
+            name: "Kieran Miller"
+          }}
+          navLinks={[
+            {
+              icon: function noRefCheck() {
+                return <img src={icon} />;
+              },
+              label: "Notes",
+              to: "/components/Common/MenubarN"
+            },
+            {
+              icon: function noRefCheck() {
+                return <UserCircle />;
+              },
+              label: "Contact",
+              to: "/components/Common/MenubarC"
+            },
+            {
+              label: "Settings",
+              to: "/",
+              icon: function noRefCheck() {
+                return <img src={inbox} />;
+              }
+            }
+          ]}
+        />
+        <div className="relative flex flex-col flex-grow h-screen overflow-auto">
+          <Switch>
+            <Route
+              component={function noRefCheck() {
+                return <MenubarN />;
+              }}
+              path="/components/Common/MenubarN"
+            />
+            <Route
+              component={function noRefCheck() {
+                return <MenubarC />;
+              }}
+              path="/components/Common/MenubarC"
+            />
+          </Switch>
         </div>
       </div>
-      <div className="flex flex-col items-center justify-between w-full h-full">
-        <div className="flex flex-col items-center justify-start w-full pt-4">
-          <NavItem title="Notes" link="/notes" icon="ri-file-text-line" />
-          <NavItem
-            title="Settings"
-            link="/settings"
-            icon="ri-settings-2-line"
-            subLinks={[
-              { title: "Change password", link: "/my/password/edit" },
-              { title: "My Profile", link: "/my/profile" }
-            ]}
-          />
-        </div>
-        <div className="mb-4">
-          <AccountDropdown handleLogout={handleLogout} />
-        </div>
-      </div>
-    </div>
+    </BrowserRouter>
+
+    // <div className="bg-gray-100 nh-sidebar" key="sidebar">
+    //   <div className="nh-logo">
+    //     <div className="flex items-center justify-center w-8 h-8 rounded-md">
+    //       <i className="text-purple-500 ri-flashlight-fill ri-2x" />
+    //     </div>
+    //   </div>
+    //   <div className="flex flex-col items-center justify-between w-full h-full">
+    //     <div className="flex flex-col items-center justify-start w-full pt-4">
+    //       <NavItem title="Notes" link="/notes" icon="ri-file-text-line" />
+    //       <NavItem
+    //         title="Settings"
+    //         link="/settings"
+    //         icon="ri-settings-2-line"
+    //         subLinks={[
+    //           { title: "Change password", link: "/my/password/edit" },
+    //           { title: "My Profile", link: "/my/profile" }
+    //         ]}
+    //       />
+    //       <UserCircle />
+    //     </div>
+    //     <div className="mb-4">
+    //       <AccountDropdown handleLogout={handleLogout} />
+    //     </div>
+    //   </div>
+    // </div>
   );
 };
 
