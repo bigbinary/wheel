7624229330633e5537fa8dc8ec8148ea3829ae9a
Divya Jomi <divyajomi01@gmail.com>
2021-10-18 22:04:40 +0530

    Created Side Bar

--- app/javascript/src/components/Common/Navbar/index.jsx
+++ app/javascript/src/components/Common/Navbar/index.jsx
@@ -1,14 +1,16 @@
 import React from "react";
 
+import { Keyboard, Settings, UserCircle } from "@bigbinary/neeto-icons";
 import { Toastr } from "neetoui";
+import { Sidebar } from "neetoui/v2/layouts";
 import { withRouter } from "react-router-dom";
 
 import authenticationApi from "apis/authentication";
 import { resetAuthTokens } from "apis/axios";
 import { useAuthDispatch } from "contexts/auth";
 
-import AccountDropdown from "./AccountDropdown";
-import NavItem from "./NavItem";
+//import AccountDropdown from "./AccountDropdown";
+//import NavItem from "./NavItem";
 
 const NavBar = () => {
   const authDispatch = useAuthDispatch();
@@ -24,29 +26,50 @@ const NavBar = () => {
   };
 
   return (
-    <div className="bg-gray-100 nh-sidebar" key="sidebar">
-      <div className="nh-logo">
-        <div className="flex items-center justify-center w-8 h-8 rounded-md">
-          <i className="text-purple-500 ri-flashlight-fill ri-2x" />
-        </div>
-      </div>
-      <div className="flex flex-col items-center justify-between w-full h-full">
-        <div className="flex flex-col items-center justify-start w-full pt-4">
-          <NavItem title="Notes" link="/notes" icon="ri-file-text-line" />
-          <NavItem
-            title="Settings"
-            link="/settings"
-            icon="ri-settings-2-line"
-            subLinks={[
-              { title: "Change password", link: "/my/password/edit" },
-              { title: "My Profile", link: "/my/profile" }
-            ]}
-          />
-        </div>
-        <div className="mb-4">
-          <AccountDropdown handleLogout={handleLogout} />
-        </div>
-      </div>
+    <div className="flex flex-row items-start justify-start">
+      <Sidebar
+        appName=""
+        collapsible
+        isCollapsed
+        navLinks={[
+          {
+            icon: function noRefCheck() {
+              return <Keyboard />;
+            },
+            label: "Notes",
+            to: "/notes"
+          },
+          {
+            icon: function noRefCheck() {
+              return <UserCircle />;
+            },
+            label: "Contacts",
+            to: "/contacts"
+          },
+          {
+            icon: function noRefCheck() {
+              return <Settings />;
+            },
+            label: "Settings",
+            to: "/settings"
+          }
+        ]}
+        profileInfo={{
+          dropdownProps: [
+            {
+              label: "Edit",
+              onClick: null
+            },
+            {
+              label: "Logout",
+              onClick: { handleLogout }
+            }
+          ],
+          email: "oliver.smith@gmail.com",
+          imageUrl: "https://randomuser.me/api/portraits/women/90.jpg",
+          name: "Kieran Miller"
+        }}
+      />
     </div>
   );
 };
