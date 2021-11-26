ad6fa85b035139ff7d39e95706cb80cb6cf74c80
Edwin Babu <edwinbbu@gmail.com>
2021-11-26 13:21:59 +0530

    neetoui major version migration (#734)
    * neetoui version update and css bundle file added
    
    * Notes table height fixed
    
    * Removed ref implementation
    
    * scss folder stucture updated

--- app/javascript/src/components/Dashboard/Notes/NoteTable.jsx
+++ app/javascript/src/components/Dashboard/Notes/NoteTable.jsx
@@ -26,10 +26,9 @@ export default function NoteTable({
 }) {
   const [showEditNote, setShowEditNote] = useState(false);
   const [selectedNote, setSelectedNote] = useState({});
-
   return (
     <>
-      <div className="w-full">
+      <div className="w-full notes-table-height">
         <Table
           rowData={notes}
           columnData={COLUMN_DATA}
