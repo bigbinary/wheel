764ff1869f998f7568442891c08faa2ab82a118c
rishabhanand12 <mailrishabh.ra@gmail.com>
2022-02-04 20:31:25 +0530

    added alert in Email form

--- app/javascript/src/components/Dashboard/Settings/Email.jsx
+++ app/javascript/src/components/Dashboard/Settings/Email.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 
 import { Form, Formik } from "formik";
-import { Button } from "neetoui";
+import { Button, Alert } from "neetoui";
 import { Input } from "neetoui/formik";
 import { Container, Header } from "neetoui/layouts";
 
@@ -11,8 +11,10 @@ import { useAuthDispatch } from "contexts/auth";
 import { useUserState } from "contexts/user";
 
 import { EMAIL_FORM_VALIDATION_SCHEMA } from "./constants";
+import { buildEmailFormInitialValues } from "./utils";
 
 const Email = () => {
+  const [showUpdateAlert, setShowUpdateAlert] = useState(false);
   const [submitted, setSubmitted] = useState(false);
   const { user } = useUserState();
   const authDispatch = useAuthDispatch();
@@ -23,7 +25,17 @@ const Email = () => {
       authDispatch({ type: "LOGOUT" });
       window.location.href = LOGIN_PATH;
     } catch (err) {
+      setShowUpdateAlert(false);
       logger.error(err);
+    }
+  };
+
+  const showUpdateConfirmation = async (e, validateForm) => {
+    e.preventDefault();
+    setSubmitted(true);
+    const errors = await validateForm();
+    if (Object.keys(errors).length < 1) {
+      setShowUpdateAlert(true);
     }
   };
 
@@ -32,13 +44,13 @@ const Email = () => {
       <Header title="Update Email" className="border-b border-gray-200" />
       <div className="mx-auto flex h-full w-full flex-col items-center justify-center sm:max-w-md">
         <Formik
-          initialValues={{ email: user.email }}
+          initialValues={buildEmailFormInitialValues(user)}
           onSubmit={handleSubmit}
           validateOnBlur={submitted}
           validateOnChange={submitted}
           validationSchema={EMAIL_FORM_VALIDATION_SCHEMA}
         >
-          {({ isSubmitting }) => (
+          {({ isSubmitting, handleSubmit: submitForm, validateForm }) => (
             <Form className="w-full space-y-6 rounded-lg border bg-white p-8 shadow-sm">
               <Input required name="email" label="Email" type="email" />
               <Input
@@ -50,9 +62,18 @@ const Email = () => {
               <Button
                 fullWidth
                 type="submit"
-                onClick={() => setSubmitted(true)}
+                onClick={e => showUpdateConfirmation(e, validateForm)}
                 label="Update"
                 className="h-8"
+                loading={isSubmitting}
+                disabled={isSubmitting}
+              />
+              <Alert
+                isOpen={showUpdateAlert}
+                message="Are you sure you want to continue? You will need to login to continue."
+                onClose={() => setShowUpdateAlert(false)}
+                onSubmit={submitForm}
+                title="You will be logged out upon updating your email!"
                 loading={isSubmitting}
                 disabled={isSubmitting}
               />
