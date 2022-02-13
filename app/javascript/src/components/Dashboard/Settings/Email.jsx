1a96ff1183f0fea22cf8a6fedce610b141ef2635
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2022-02-13 13:06:13 +0530

    Fixed Email tab issues and cleaned it up

--- app/javascript/src/components/Dashboard/Settings/Email.jsx
+++ app/javascript/src/components/Dashboard/Settings/Email.jsx
@@ -1,24 +1,24 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 
 import { Form, Formik } from "formik";
 import { Button } from "neetoui";
 import { Input } from "neetoui/formik";
 import { Container, Header } from "neetoui/layouts";
-import { equals } from "ramda";
 
 import profilesApi from "apis/profiles";
 import { LOGIN_PATH } from "components/routeConstants";
 import { useAuthDispatch } from "contexts/auth";
 import { useUserState } from "contexts/user";
 
-import ConfirmPasswordModal from "./ConfirmPasswordModal";
+import ConfirmPasswordFormModal from "./ConfirmPasswordFormModal";
 import { EMAIL_FORM_VALIDATION_SCHEMA } from "./constants";
+import { useFormikPasswordConfirmationModal } from "./hooks/useFormikPasswordConfirmationModal";
 import { buildEmailFormInitialValues } from "./utils";
 
 const Email = () => {
-  const [showPasswordModal, setShowPasswordModal] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
+  const formikPasswordContext = useFormikPasswordConfirmationModal();
   const { user } = useUserState();
+
   const authDispatch = useAuthDispatch();
   const initialFormValues = useMemo(
     () => buildEmailFormInitialValues(user),
@@ -27,29 +27,23 @@ const Email = () => {
 
   const handleSubmit = async (data, { resetForm }) => {
     try {
-      await profilesApi.updateEmail(data);
+      await profilesApi.updateEmail({
+        ...data,
+        password: formikPasswordContext.password,
+      });
       authDispatch({ type: "LOGOUT" });
       window.location.href = LOGIN_PATH;
     } catch (err) {
       resetForm();
-      setShowPasswordModal(false);
+      formikPasswordContext.setShowPasswordModal(false);
       logger.error(err);
     }
   };
 
   const promptPassword = async (e, validateForm) => {
     e.preventDefault();
-    setSubmitted(true);
     const { email } = await validateForm();
-    if (!email) setShowPasswordModal(true);
-  };
-
-  const closeModal = (values, resetForm) => {
-    setShowPasswordModal(false);
-    resetForm({
-      values: { ...values, password: "" },
-      errors: {},
-    });
+    if (!email) formikPasswordContext.setShowPasswordModal(true);
   };
 
   return (
@@ -59,17 +53,10 @@ const Email = () => {
         <Formik
           initialValues={initialFormValues}
           onSubmit={handleSubmit}
-          validateOnBlur={submitted}
-          validateOnChange={submitted}
+          innerRef={formikPasswordContext.formRef}
           validationSchema={EMAIL_FORM_VALIDATION_SCHEMA}
         >
-          {({
-            values,
-            isSubmitting,
-            handleSubmit: submitForm,
-            validateForm,
-            resetForm,
-          }) => (
+          {({ dirty, isSubmitting, validateForm }) => (
             <Form className="w-full space-y-6 rounded-lg border bg-white p-8 shadow-sm">
               <Input required name="email" label="Email" type="email" />
               <Button
@@ -79,33 +66,18 @@ const Email = () => {
                 label="Update"
                 className="h-8"
                 loading={isSubmitting}
-                disabled={equals(values, initialFormValues) || isSubmitting}
+                disabled={!dirty || isSubmitting}
               />
-              <ConfirmPasswordModal
-                isOpen={showPasswordModal}
-                onClose={() => closeModal(values, resetForm)}
-                header="Are you sure you want to update your email?"
-                onSubmit={submitForm}
-                isSubmitting={isSubmitting}
-                disabled={isSubmitting || !values.password}
-              >
-                <>
-                  <p className="my-2">
-                    You will be logged out upon updating your email! Please
-                    enter your password to continue.
-                  </p>
-                  <Input
-                    required
-                    name="password"
-                    label="Current password"
-                    type="password"
-                    className="my-2"
-                  />
-                </>
-              </ConfirmPasswordModal>
             </Form>
           )}
         </Formik>
+        <ConfirmPasswordFormModal
+          isOpen={formikPasswordContext.showPasswordModal}
+          onClose={formikPasswordContext.closeModal}
+          header="Are you sure you want to update your email?"
+          onSubmit={formikPasswordContext.handlePasswordConfirmation}
+          alertMessage="You will be logged out upon updating your email! Please enter your password to continue."
+        />
       </div>
     </Container>
   );
