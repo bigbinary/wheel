416c6007eb82a123cb536f487d60fd3173c31346
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2022-02-13 13:06:03 +0530

    Fixed Profile tab issues and cleaned it up

--- app/javascript/src/components/Dashboard/Settings/Profile.jsx
+++ app/javascript/src/components/Dashboard/Settings/Profile.jsx
@@ -1,61 +1,49 @@
-import React, { useMemo, useState } from "react";
+import React, { useMemo } from "react";
 
 import { Form, Formik } from "formik";
 import { Button } from "neetoui";
 import { Input } from "neetoui/formik";
 import { Container, Header } from "neetoui/layouts";
-import { equals } from "ramda";
 
 import profilesApi from "apis/profiles";
 import { useUserState } from "contexts/user";
 import { useUserDispatch } from "contexts/user";
 
-import ConfirmPasswordModal from "./ConfirmPasswordModal";
+import ConfirmPasswordFormModal from "./ConfirmPasswordFormModal";
 import { PROFILE_FORM_VALIDATION_SCHEMA } from "./constants";
+import { useFormikPasswordConfirmationModal } from "./hooks/useFormikPasswordConfirmationModal";
 import { buildProfileFormInitialValues } from "./utils";
 
 const Profile = () => {
-  const [showPasswordModal, setShowPasswordModal] = useState(false);
-  const [submitted, setSubmitted] = useState(false);
+  const formikPasswordContext = useFormikPasswordConfirmationModal();
   const { user } = useUserState();
+
   const userDispatch = useUserDispatch();
   const initialFormValues = useMemo(
     () => buildProfileFormInitialValues(user),
     [user]
   );
 
-  const handleSubmit = async (data, { resetForm }) => {
+  const handleSubmit = async values => {
     try {
       const {
         data: { user },
-      } = await profilesApi.updateProfile(data);
+      } = await profilesApi.updateProfile({
+        ...values,
+        password: formikPasswordContext.password,
+      });
       userDispatch({ type: "SET_USER", payload: { user } });
     } catch (err) {
       logger.error(err);
-    } finally {
-      resetForm({
-        values: buildProfileFormInitialValues({
-          first_name: data.firstName,
-          last_name: data.lastName,
-        }),
-      });
-      setShowPasswordModal(false);
     }
   };
 
   const promptPassword = async (e, validateForm) => {
     e.preventDefault();
-    setSubmitted(true);
     const { firstName, lastName } = await validateForm();
-    if (!firstName && !lastName) setShowPasswordModal(true);
-  };
-
-  const closeModal = (values, resetForm) => {
-    setShowPasswordModal(false);
-    resetForm({
-      values: { ...values, password: "" },
-      errors: {},
-    });
+    if (!firstName && !lastName) {
+      formikPasswordContext.setShowPasswordModal(true);
+    }
   };
 
   return (
@@ -63,19 +51,12 @@ const Profile = () => {
       <Header title="My Profile" className="border-b border-gray-200" />
       <div className="mx-auto flex h-full w-full flex-col items-center justify-center sm:max-w-md">
         <Formik
+          innerRef={formikPasswordContext.formRef}
           initialValues={initialFormValues}
           onSubmit={handleSubmit}
-          validateOnBlur={submitted}
-          validateOnChange={submitted}
           validationSchema={PROFILE_FORM_VALIDATION_SCHEMA}
         >
-          {({
-            values,
-            isSubmitting,
-            handleSubmit: submitForm,
-            validateForm,
-            resetForm,
-          }) => (
+          {({ dirty, isSubmitting, validateForm }) => (
             <Form className="w-full space-y-6 rounded-lg border bg-white p-8 shadow-sm">
               <Input required name="firstName" label="First Name" />
               <Input required name="lastName" label="Last name" />
@@ -86,27 +67,17 @@ const Profile = () => {
                 label="Update"
                 className="h-8"
                 loading={isSubmitting}
-                disabled={equals(values, initialFormValues) || isSubmitting}
+                disabled={!dirty || isSubmitting}
               />
-              <ConfirmPasswordModal
-                isOpen={showPasswordModal}
-                onClose={() => closeModal(values, resetForm)}
-                header="Please enter your password to continue."
-                onSubmit={submitForm}
-                isSubmitting={isSubmitting}
-                disabled={isSubmitting || !values.password}
-              >
-                <Input
-                  required
-                  name="password"
-                  label="Current password"
-                  type="password"
-                  className="my-2"
-                />
-              </ConfirmPasswordModal>
             </Form>
           )}
         </Formik>
+        <ConfirmPasswordFormModal
+          isOpen={formikPasswordContext.showPasswordModal}
+          onClose={formikPasswordContext.closeModal}
+          header="Please enter your password to continue."
+          onSubmit={formikPasswordContext.handlePasswordConfirmation}
+        />
       </div>
     </Container>
   );
