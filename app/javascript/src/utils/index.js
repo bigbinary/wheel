09b97954136ac62ff4a39f9f87a39677f4aed282
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2022-04-21 15:32:53 +0530

    Revert back to using Ramda (#829)
    * Revert "Removed ramda.js package and replaced with vanilla JS helpers (#817)"
    
    This reverts commit 57ceb693b77d655cb2379ed1639a988405ac9b8c.
    
    * Created an isPresent helper using Ramda

--- app/javascript/src/utils/index.js
+++ app/javascript/src/utils/index.js
@@ -1,20 +1,3 @@
-const isEmpty = obj => {
-  // eslint-disable-next-line eqeqeq
-  if (obj == null) return true;
+import * as R from "ramda";
 
-  if (typeof obj === "number") return false;
-
-  if (typeof obj === "string" || obj instanceof String) return obj === "";
-
-  if (Array.isArray(obj)) return obj?.length === 0 || false;
-
-  if (typeof obj === "object") {
-    const isKeyValuePairEmpty = true;
-    for (const _ in obj) return !isKeyValuePairEmpty;
-    return isKeyValuePairEmpty;
-  }
-
-  return !!obj;
-};
-
-export const isPresent = obj => !isEmpty(obj);
+export const isPresent = R.pipe(R.either(R.isNil, R.isEmpty), R.not);
