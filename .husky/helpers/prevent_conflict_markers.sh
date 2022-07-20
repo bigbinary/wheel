1b64346ae06e96e1bce84a67baf5e3856b826443
Bhavik Parmar <38880921+bhavik2936@users.noreply.github.com>
2022-07-20 14:00:45 +0530

    Fix Husky pre-commit Error (#856)
    * modify exeuction to bash
    
    * Revert "modify exeuction to bash"
    
    This reverts commit f56ed73c4bed686a87f16f580d5848cee73f935a.
    
    * modify to while loop
    
    * modify to integer
    
    * modify shebang

--- .husky/helpers/prevent_conflict_markers.sh
+++ .husky/helpers/prevent_conflict_markers.sh
@@ -1,12 +1,15 @@
-#!/bin/bash
+#!/usr/bin/env sh
 
 repeat() {
   char="$1"
   times="$2"
   text=""
-  for (( i = 0; i < "$times"; ++i ))
+  i=0
+
+  while [ $i -lt $times ]
   do
     text="${text}${char}"
+    i=$((i+1))
   done
   echo "$text"
 }
