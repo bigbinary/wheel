906f30079eea722743aab8a1d368573c18c10eeb
Vikas Bishnoi <vikaskbm@gmail.com>
2022-09-12 16:39:38 +0530

    Removed table component and added delete note UI and functionality

--- app/javascript/src/components/Dashboard/Notes/Note.jsx
+++ app/javascript/src/components/Dashboard/Notes/Note.jsx
@@ -3,40 +3,58 @@ import React from "react";
 import { MenuVertical, Clock } from "@bigbinary/neeto-icons";
 import { Typography, Dropdown, Tag, Tooltip, Avatar } from "neetoui";
 
-const Note = ({ title, description, tag, status, created_at, avatar }) => (
-  <div className="mb-3 w-full border p-4 shadow-md">
-    <div className="flex justify-between">
-      <Typography component="h4">{title}</Typography>
-      <Dropdown buttonStyle="text" icon={MenuVertical} position="bottom-end">
-        <li>Edit</li>
-        <li>Delete</li>
-      </Dropdown>
-    </div>
-    <div className="mb-3">
-      <Typography style="body2" weight="light">
-        {description}
-      </Typography>
-    </div>
-    <hr />
-    <div className="mt-3 flex justify-between">
-      <Tag className="capitalize" color="gray" label={tag} size="small" />
-      <div className="flex items-center space-x-1.5">
-        <Clock size={14} />
-        <Tooltip content="Friday, 05:36PM" position="bottom">
-          <Typography className="capitalize" style="body2">
-            {status} {created_at}
-          </Typography>
-        </Tooltip>
-        <Avatar
-          size="small"
-          user={{
-            imageUrl: avatar,
-          }}
-          onClick={function noRefCheck() {}}
-        />
+const Note = ({
+  id,
+  title,
+  description,
+  tag,
+  status,
+  created_at,
+  avatar,
+  setShowDeleteAlert,
+  setDeleteNoteId,
+  setIsOpen,
+}) => {
+  const deleteHelper = () => {
+    setIsOpen(true);
+    setShowDeleteAlert(true);
+    setDeleteNoteId(id);
+  };
+  return (
+    <div className="mb-3 w-full border p-4 shadow-md">
+      <div className="flex justify-between">
+        <Typography component="h4">{title}</Typography>
+        <Dropdown buttonStyle="text" icon={MenuVertical} position="bottom-end">
+          <li>Edit</li>
+          <li onClick={deleteHelper}>Delete</li>
+        </Dropdown>
+      </div>
+      <div className="mb-3">
+        <Typography style="body2" weight="light">
+          {description}
+        </Typography>
+      </div>
+      <hr />
+      <div className="mt-3 flex justify-between">
+        <Tag className="capitalize" color="gray" label={tag} size="small" />
+        <div className="flex items-center space-x-1.5">
+          <Clock size={14} />
+          <Tooltip content="Friday, 05:36PM" position="bottom">
+            <Typography className="capitalize" style="body2">
+              {status} {created_at}
+            </Typography>
+          </Tooltip>
+          <Avatar
+            size="small"
+            user={{
+              imageUrl: avatar,
+            }}
+            onClick={function noRefCheck() {}}
+          />
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Note;
--- app/javascript/src/components/Dashboard/Notes/index.jsx
+++ app/javascript/src/components/Dashboard/Notes/index.jsx
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 
-import { Button, PageLoader } from "neetoui";
+import { Button, PageLoader, Toastr } from "neetoui";
 import { Container, Header } from "neetoui/layouts";
 
 import { NOTES } from "./constants";
+import DeleteAlert from "./DeleteAlert";
 import NotesList from "./NotesList";
 import NotesMenu from "./NotesMenu";
 import NewNotePane from "./Pane/Create";
@@ -14,6 +15,9 @@ const Notes = () => {
   const [searchTerm, setSearchTerm] = useState("");
   const [notes, setNotes] = useState([{}]);
   const [showMenu, setShowMenu] = useState(false);
+  const [showDeleteAlert, setShowDeleteAlert] = useState(false);
+  const [deleteNoteId, setDeleteNoteId] = useState(null);
+  const [isOpen, setIsOpen] = useState(true);
 
   useEffect(() => {
     setNotes(NOTES);
@@ -23,6 +27,23 @@ const Notes = () => {
   if (loading) {
     return <PageLoader />;
   }
+
+  const handleDeleteNote = async () => {
+    try {
+      const newNotes = notes.filter(note => note.id !== deleteNoteId);
+      setNotes(newNotes);
+      setIsOpen(false);
+      Toastr.success("Note Successfully Submitted");
+    } catch (error) {
+      logger.error(error);
+      Toastr.success("Some Error Occured! Note not deleted");
+    }
+  };
+
+  const closeDeleteAlertHelper = () => {
+    setDeleteNoteId(null);
+    setShowDeleteAlert(false);
+  };
 
   return (
     <Container>
@@ -44,16 +65,24 @@ const Notes = () => {
           onChange: e => setSearchTerm(e.target.value),
         }}
       />
-      <NotesList notes={notes} setShowNewNotePane={setShowNewNotePane} />
+      <NotesList
+        notes={notes}
+        setDeleteNoteId={setDeleteNoteId}
+        setIsOpen={setIsOpen}
+        setShowDeleteAlert={setShowDeleteAlert}
+        setShowNewNotePane={setShowNewNotePane}
+      />
       <NewNotePane
         setShowPane={setShowNewNotePane}
         showPane={showNewNotePane}
       />
-      {/* {showDeleteAlert && (
+      {showDeleteAlert && (
         <DeleteAlert
-          onClose={() => setShowDeleteAlert(false)}
+          isOpen={isOpen}
+          onClose={closeDeleteAlertHelper}
+          onDelete={handleDeleteNote}
         />
-      )} */}
+      )}
     </Container>
   );
 };
