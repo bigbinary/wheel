f397b79529f94602f644dcc8547032e7978d23f9
Yedhin Kizhakkethara <yedhin1998@gmail.com>
2022-09-15 21:05:02 +0530

    Ignore TypeScript declaration types from Webpack build (#893)
    * Ignore TypeScript declaration types from Webpack build
    
    * Add empty line after require

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,6 +1,23 @@
 const { environment } = require("@rails/webpacker");
 
 const customAliasConfig = require("./alias");
+
 environment.config.merge(customAliasConfig);
+
+const babelLoader = environment.loaders.get("babel");
+babelLoader.exclude = [/\.(d.ts)$/i];
+
+environment.loaders.insert(
+  "ignore",
+  {
+    test: /\.(d.ts)$/,
+    use: babelLoader.use.concat([
+      {
+        loader: "ignore-loader",
+      },
+    ]),
+  },
+  { before: "file" }
+);
 
 module.exports = environment;
