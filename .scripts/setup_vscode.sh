c3ad23434947a58fe5653283d960f090f2dfcc35
Udai Gupta <udaigupta19311@gmail.com>
2022-11-09 17:11:08 +0530

    Added check for code command

--- .scripts/setup_vscode.sh
+++ .scripts/setup_vscode.sh
@@ -2,10 +2,18 @@
 
 require "json"
 
+code_file_location = `which codes`
+code_insiders_file_location = `which code-insiders`
+
+if code_file_location.empty? && code_insiders_file_location.empty?
+  puts "Couldn't find an executable for VSCode. Please ensure that the code or code-insiders command is available in your PATH env"
+  return
+end
+
 config = `curl -s 'https://raw.githubusercontent.com/bigbinary/wheel/main/.vscode/extensions.json'`
 extensions = JSON.parse(config)["recommendations"]
 
 extensions.each do |extension|
-  output = `code --install-extension #{extension}`
+  output = code_file_location.empty? ? `code-insiders --install-extension #{extension}` : `code --install-extension #{extension}`
   puts output
 end
