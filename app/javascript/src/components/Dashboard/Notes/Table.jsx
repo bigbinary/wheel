a15536d1cbda3617d0e152fc2579ce0f05496c54
Datt Dongare <dattatraya.dongare@bigbinary.com>
2022-11-17 17:51:01 +0530

    Fix duplicate columns

--- app/javascript/src/components/Dashboard/Notes/Table.jsx
+++ app/javascript/src/components/Dashboard/Notes/Table.jsx
@@ -5,15 +5,15 @@ import { Table as NeetoUITable } from "neetoui";
 import Card from "./Card";
 
 const Table = ({ notes = [] }) => {
-  const columnData = notes.map(note => ({
-    key: note.title,
-    dataIndex: note.title,
-    render: () => <Card note={note} />,
-  }));
+  const columnData = [
+    {
+      render: note => <Card note={note} />,
+    },
+  ];
 
   return (
     <div className="notes-table-height w-full">
-      <NeetoUITable allowRowClick columnData={columnData} rowData={notes} />
+      <NeetoUITable columnData={columnData} rowData={notes} />
     </div>
   );
 };
