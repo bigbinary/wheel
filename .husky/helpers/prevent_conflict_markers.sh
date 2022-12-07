edf2e07fb3879a9d6bf214a60078f4f4ca604017
Udai Gupta <43845165+udai1931@users.noreply.github.com>
2022-12-07 16:29:46 +0530

    Husky conflict markers hook fix (#932)

--- .husky/helpers/prevent_conflict_markers.sh
+++ .husky/helpers/prevent_conflict_markers.sh
@@ -22,7 +22,7 @@ prevent_conflict_markers() {
   right_arrow=$(repeat '>' 7)
   equal_symbol=$(repeat '=' 7)
 
-  CONFLICT_MARKERS="${left_arrow}|${equal_symbol}|${right_arrow}"
+  CONFLICT_MARKERS="^${left_arrow}$|^${equal_symbol}$|^${right_arrow}$"
   occurrences_count=$(git --no-pager diff --staged --ignore-blank-lines --name-only -G"$CONFLICT_MARKERS" | wc -l)
   if [ "$occurrences_count" -gt 0 ]
   then
