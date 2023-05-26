17e75d08fdd868568b95303191495e7a4d54991c
Udai Gupta <43845165+udai1931@users.noreply.github.com>
2023-05-26 16:58:54 +0530

    Updated run_eslint_on_modified_files script (#970)

--- .semaphore/commands/run_eslint_on_modified_files.sh
+++ .semaphore/commands/run_eslint_on_modified_files.sh
@@ -1,5 +1,4 @@
-checkout
 git fetch --unshallow 2> /dev/null
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin main
-git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | grep --color=none -i -e "\.js$" -e "\.jsx$" | xargs npx eslint
+git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | grep --color=none -i -e "\.js$" -e "\.jsx$"| awk '!/.eslintrc.js/ {print}' | xargs npx eslint --no-error-on-unmatched-pattern
