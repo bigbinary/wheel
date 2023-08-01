b980f3a25eed41f64da8cf0626fb87e5ce7031ba
Sreeram Venkitesh <40194401+sreeram-venkitesh@users.noreply.github.com>
2023-08-01 11:16:20 +0530

    Fixed typo with `File.exists?` (#989)

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -20,7 +20,7 @@ Sidekiq.configure_server do |config|
 
   schedule_file = Rails.root.join("config/schedule.yml")
 
-  if File.exists?(schedule_file)
+  if File.exist?(schedule_file)
     Sidekiq::Cron::Job.load_from_hash! YAML.load_file(schedule_file)
   end
 end
