901da1639345d05cd6551c9b342bd48807b64004
S Varun <47653646+VarunSriram99@users.noreply.github.com>
2023-08-28 15:13:48 +0530

    Added currents configuration (#993)

---
+++ cypress-tests/currents.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  projectId: "D3qN3R",
+  /**
+   * We can just add the director service URL here if we decide to use a self hosted version of Sorry Cypress
+   * cloudServiceUrl: "http://localhost:1234",
+   */
+  e2e: { batchSize: 1 },
+};
