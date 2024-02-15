3831af905edf8e5ae3a50e5d777080260dd6e82b
Sruthy J Mallya <53861843+sruthy-j-mallya@users.noreply.github.com>
2024-02-15 16:11:11 +0530

    Utilized actionblock for formik buttons (#1026)

--- app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
+++ app/javascript/src/components/Dashboard/Notes/Pane/Form.jsx
@@ -1,8 +1,8 @@
 import React from "react";
 
 import { Formik, Form as FormikForm } from "formik";
-import { Button, Pane } from "neetoui";
-import { Input, Textarea } from "neetoui/formik";
+import { Pane } from "neetoui";
+import { ActionBlock, Input, Textarea } from "neetoui/formik";
 
 import notesApi from "apis/notes";
 
@@ -29,36 +29,33 @@ const Form = ({ onClose, refetch, note, isEdit }) => {
       validationSchema={NOTES_FORM_VALIDATION_SCHEMA}
       onSubmit={handleSubmit}
     >
-      {({ isSubmitting }) => (
-        <FormikForm className="w-full">
-          <Pane.Body className="space-y-6">
-            <Input
-              required
-              className="w-full flex-grow-0"
-              label="Title"
-              name="title"
-            />
-            <Textarea
-              required
-              className="w-full flex-grow-0"
-              label="Description"
-              name="description"
-              rows={8}
-            />
-          </Pane.Body>
-          <Pane.Footer>
-            <Button
-              className="mr-3"
-              disabled={isSubmitting}
-              label={isEdit ? "Update" : "Save changes"}
-              loading={isSubmitting}
-              style="primary"
-              type="submit"
-            />
-            <Button label="Cancel" style="text" onClick={onClose} />
-          </Pane.Footer>
-        </FormikForm>
-      )}
+      <FormikForm className="w-full">
+        <Pane.Body className="space-y-6">
+          <Input
+            required
+            className="w-full flex-grow-0"
+            label="Title"
+            name="title"
+          />
+          <Textarea
+            required
+            className="w-full flex-grow-0"
+            label="Description"
+            name="description"
+            rows={8}
+          />
+        </Pane.Body>
+        <Pane.Footer>
+          <ActionBlock
+            cancelButtonProps={{
+              onClick: onClose,
+            }}
+            submitButtonProps={{
+              className: "mr-3",
+            }}
+          />
+        </Pane.Footer>
+      </FormikForm>
     </Formik>
   );
 };
