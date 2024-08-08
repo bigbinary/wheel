0d5ea7b7547a5b3a56cb77dfc7e29b08277abc88
Abhay V Ashokan <abhayvashokan@gmail.com>
2024-08-08 19:30:55 +0530

    Updated the script to only check for Rubocop failures

--- .scripts/run_rubocop_and_erblint_on_modified_files.sh
+++ .scripts/run_rubocop_and_erblint_on_modified_files.sh
@@ -1,7 +1,5 @@
 git fetch --unshallow 2> /dev/null
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin main
-new_files=$(git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main)
-echo "$new_files" | xargs bundle exec rubocop --force-exclusion
-echo "$new_files" | xargs bundle exec erblint --lint-all --format compact
+git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | xargs bundle exec rubocop --force-exclusion
 
