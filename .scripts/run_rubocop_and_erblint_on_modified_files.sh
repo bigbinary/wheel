6684b086f7e1c31c0f6cb2ae8a0ef94e4ffd2b57
Abhay V Ashokan <abhayvashokan@gmail.com>
2024-08-12 14:01:03 +0530

    Do not execute RuboCop if there are no file changes

--- .scripts/run_rubocop_and_erblint_on_modified_files.sh
+++ .scripts/run_rubocop_and_erblint_on_modified_files.sh
@@ -1,5 +1,12 @@
 git fetch --unshallow 2> /dev/null
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin main
-git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | xargs bundle exec rubocop --force-exclusion
+
+CHANGED_FILES=$(git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main)
+
+if [ -n "$CHANGED_FILES" ]; then
+  echo "$CHANGED_FILES" | xargs bundle exec rubocop --force-exclusion
+else
+  echo "No file changes detected. Skipping RuboCop."
+fi
 
