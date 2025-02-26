b2df29d57fb289c67a2290972f3f817fd6fdce00
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-02-26 09:11:36 +0530

    Minor fix in logger.js

--- app/javascript/src/common/logger.js
+++ app/javascript/src/common/logger.js
@@ -1,8 +1,9 @@
+import Logger from "js-logger";
+
 export const initializeLogger = () => {
   /* eslint react-hooks/rules-of-hooks: "off" */
-  const Logger = require("js-logger");
   Logger.useDefaults();
-  if (process.env.RAILS_ENV === "production") {
+  if (import.meta.env.MODE === "production") {
     Logger.setLevel(Logger.OFF);
   }
 };
