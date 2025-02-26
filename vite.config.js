a17226f8ae6c49fa3b88c771cf5361d826526534
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-02-26 09:05:55 +0530

    Added alias to vite config

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 const postCssConfig = require("./postcss.config");
 
 const port = process.env.DEVSERVER_PORT || 8000;
@@ -12,6 +14,18 @@ const config = {
     cssCodeSplit: false,
   },
   root: "app/javascript/packs",
+  resolve: {
+    alias: {
+      contexts: path.resolve(__dirname, "./app/javascript/src/contexts"),
+      components: path.resolve(__dirname, "./app/javascript/src/components"),
+      reducers: path.resolve(__dirname, "./app/javascript/src/reducers"),
+      common: path.resolve(__dirname, "./app/javascript/src/common"),
+      apis: path.resolve(__dirname, "./app/javascript/src/apis"),
+      utils: path.resolve(__dirname, "./app/javascript/src/utils"),
+      neetoui: "@bigbinary/neetoui",
+      neetoicons: "@bigbinary/neetoicons",
+    },
+  },
 };
 
 module.exports = config;
