86d544f8d875c2661a1bb9b23d79ac55cc22bdd5
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-02-26 14:54:39 +0530

    Changed MODE to ENV in logger.js

--- app/javascript/src/common/logger.js
+++ app/javascript/src/common/logger.js
@@ -3,7 +3,7 @@ import Logger from "js-logger";
 export const initializeLogger = () => {
   /* eslint react-hooks/rules-of-hooks: "off" */
   Logger.useDefaults();
-  if (import.meta.env.MODE === "production") {
+  if (import.meta.env.RAILS_ENV === "production") {
     Logger.setLevel(Logger.OFF);
   }
 };
