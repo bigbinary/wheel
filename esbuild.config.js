3c052c2ff4a4d76a3ab5d1fbb9f38a6c13b5d473
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-02-27 15:04:43 +0530

    Add necessary plugins to esbuild.config.js

--- esbuild.config.js
+++ esbuild.config.js
@@ -1,12 +1,70 @@
+import { createRequire } from "module";
+import path from "path";
+
 import { build } from "esbuild";
+import { nodeModulesPolyfillPlugin } from "esbuild-plugins-node-modules-polyfill";
+import { sassPlugin } from "esbuild-sass-plugin";
+import postcss from "postcss";
 import { mergeDeepLeft } from "ramda";
+import sass from "sass";
 
 import projectConfigurations from "./config/esbuild/config.js";
+import postCssConfig from "./postcss.config.js";
+
+const require = createRequire(import.meta.url);
+const isWatchMode = process.argv.includes("--watch");
 
 const { extensions, ...projectConfigWithoutExtensions } = projectConfigurations;
 
+const alias = {
+  images: path.resolve(process.cwd(), "app/assets/images"),
+  crypto: require.resolve("crypto-browserify"),
+  path: require.resolve("path-browserify"),
+  buffer: require.resolve("buffer"),
+  stream: require.resolve("stream-browserify"),
+};
+
 const defaultConfigurations = {
   bundle: true,
+  format: "esm",
+  platform: "browser",
+  mainFields: ["browser", "module", "main"],
+  outdir: path.join(process.cwd(), "app/assets/builds"),
+  sourcemap: isWatchMode ? true : "external",
+  loader: {
+    ".png": "file",
+    ".jpeg": "file",
+    ".jpg": "file",
+    ".svg": "file",
+    ".ico": "file",
+  },
+  plugins: [
+    sassPlugin({
+      transform: async source => {
+        const { css } = await postcss(postCssConfig.plugins).process(source, {
+          from: undefined,
+        });
+
+        return css;
+      },
+      importMapper: path =>
+        path.replace("@bigbinary/neetoui", "@bigbinary/neetoui/dist"),
+      logger: sass.Logger.silent,
+    }),
+    nodeModulesPolyfillPlugin({
+      modules: {
+        events: true,
+        fs: true,
+        process: true,
+      },
+    }),
+  ],
+  alias,
+  define: {
+    "process.env.RAILS_ENV": "'development'",
+    "process.env.NODE_DEBUG": "'development'",
+    "process.env": "{}",
+  },
 };
 
 build(mergeDeepLeft(projectConfigWithoutExtensions, defaultConfigurations));
