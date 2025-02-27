0eedd1504857324d5268497f614e2b14aca6c940
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-02-27 16:28:51 +0530

    Remove imported stylesheet and lazy loading from entrypoint

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,11 +8,12 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-import "../stylesheets/application.scss";
+// import "../stylesheets/application.scss";
 import React from "react";
 import ReactRailsUJS from "react_ujs";
 
-const App = React.lazy(() => import("../src/App"));
+// const App = React.lazy(() => import("../src/App"));
+import App from "../src/App";
 
 // Support component names relative to this directory:
 const componentsContext = { App };
