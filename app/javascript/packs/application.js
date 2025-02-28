37ae8968355e04bb7ead3a5e9ac5e087837ce4ad
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-03-01 03:13:51 +0530

    Imported application.scss in application.js

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,7 @@
 // To reference this file, add <%= javascript_pack_tag 'application' %> to the appropriate
 // layout file, like app/views/layouts/application.html.erb
 
-// import "../stylesheets/application.scss";
+import "../stylesheets/application.scss";
 import React from "react";
 import ReactRailsUJS from "react_ujs";
 
