7ff07bcfcbabe19f4cfcfad1665edec4d079fa64
Abhay V Ashokan <abhayvashokan@gmail.com>
2025-03-03 13:37:43 +0530

    Prevent scss compression in asset pipeline - it will be done by ESBuild

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
   # Compress CSS using a preprocessor.
-  # config.assets.css_compressor = :sass
+  config.assets.css_compressor = nil
 
   # Compress JS assets using a preprocessor.
   config.assets.js_compressor = Uglifier.new(harmony: true)
