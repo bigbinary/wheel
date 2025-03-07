ed5290f09758bd82b08776f79b9ac2beadafde0d
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-03-07 17:56:01 +0530

    Added babel/preset-react in babelOptions

--- .eslintrc.js
+++ .eslintrc.js
@@ -43,6 +43,9 @@ module.exports = {
     ecmaFeatures: {
       jsx: true,
     },
+    babelOptions: {
+      presets: ["@babel/preset-react"],
+    },
     ecmaVersion: 2018,
     sourceType: "module",
   },
