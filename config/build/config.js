633116c853132b49f151f95c0780143142d25d47
Vedansh Agrawal <vedansh.agrawal@bigbinary.com>
2025-03-07 15:45:48 +0530

    Created config/build/constants.js and moved all but alias to it

--- config/build/config.js
+++ config/build/config.js
@@ -1,17 +1,3 @@
-import { createRequire } from "module";
-import path from "path";
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const require = createRequire(import.meta.url);
-
-const entryPoint = file => path.join(process.cwd(), file);
-
-const absolutePath = basePath =>
-  path.resolve(__dirname, "..", "..", `app/javascript/${basePath}`);
-
 const alias = {
   images: path.resolve(process.cwd(), "app/assets/images"),
   crypto: require.resolve("crypto-browserify"),
@@ -30,32 +16,3 @@ const alias = {
   assets: absolutePath("../assets"),
 };
 
-const define = {
-  "process.env.RAILS_ENV": "'development'",
-  "process.env.NODE_DEBUG": "'development'",
-  "process.env": "{}",
-};
-
-const entryPoints = {
-  application: entryPoint("app/javascript/packs/application.js"),
-};
-
-const extensions = [
-  ".ts",
-  ".mjs",
-  ".js",
-  ".jsx",
-  ".sass",
-  ".scss",
-  ".css",
-  ".module.sass",
-  ".module.scss",
-  ".module.css",
-  ".png",
-  ".svg",
-  ".gif",
-  ".jpeg",
-  ".jpg",
-];
-
-export { alias, define, entryPoints, extensions };
