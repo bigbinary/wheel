2b170c0342e03499e5a2937f0a5ada79852b1a4a
Abhay V Ashokan <35297280+AbhayVAshokan@users.noreply.github.com>
2025-05-20 11:10:14 +0530

    Update run_eslint_on_modified_files.sh

--- .semaphore/commands/run_eslint_on_modified_files.sh
+++ .semaphore/commands/run_eslint_on_modified_files.sh
@@ -1,4 +1,11 @@
 git fetch --unshallow 2> /dev/null
 git config remote.origin.fetch '+refs/heads/*:refs/remotes/origin/*'
 git fetch origin main
-git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | grep --color=none -i -e "\.js$" -e "\.jsx$"| awk '!/eslint.config.mjs/ {print}' | xargs npx eslint --cache --no-error-on-unmatched-pattern
+
+CHANGED_FILES=$(git diff-tree --diff-filter=a -r --no-commit-id --name-only HEAD remotes/origin/main | grep --color=none -i -e "\.js$" -e "\.jsx$"| awk '!/eslint.config.mjs/ {print}')
+
+if [ -n "$CHANGED_FILES" ]; then
+  echo "$CHANGED_FILES" | xargs npx eslint --cache --no-error-on-unmatched-pattern
+else
+  echo "No file changes detected. Skipping ESlint."
+fi
