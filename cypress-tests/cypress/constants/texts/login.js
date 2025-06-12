b56a7a77d1e9327ac0ca505ad553d07bc5a2ca2b
Deepanshu Rajput <deepanshu@bigbinary.com>
2025-06-12 17:28:48 +0530

    Removed cypress directories (#1098)

--- cypress-tests/cypress/constants/texts/login.js
+++
@@ -1,7 +0,0 @@
-export const loginTexts = {
-  loginMessage: "Logged in successfully.",
-  required: "Required",
-  invalidEmailMessage: "Invalid email address",
-  incorrectEmailOrPasswordMessage: "Incorrect email or password",
-  notes: "Notes",
-};
